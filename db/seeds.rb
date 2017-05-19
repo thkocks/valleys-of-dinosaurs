a808a5444a5c458f79f40310488f5f78e133c6ea
Tim Kocks <timhkocks@gmail.com>
2017-05-19 12:05:41 +0200

    Add randomsaurus

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,3 +9,8 @@
 dino1 = Dinosaur.create!( { name: "Iguanodon", age: 3, image_url: "https://goo.gl/wB6q9q" } )
 dino2 = Dinosaur.create!( { name: "Mosasaurus", age: 10, image_url: "https://goo.gl/aoZsYt" } )
 dino3 = Dinosaur.create!( { name: "Hungarosaurus", age: 15, image_url: "https://goo.gl/EqRlgP" } )
+dino4 = Dinosaur.create!( { name: "Randomsaurus", age:rand(100), image_url:"http://pre00.deviantart.net/ebfd/th/pre/i/2013/226/d/1/rainbow_dinosaur__without_background__by_totallyrandomartist-d6i4uwy.png" })
+dino5 = Dinosaur.create!( { name: "Randomsaurus", age:rand(100), image_url:"http://pre00.deviantart.net/ebfd/th/pre/i/2013/226/d/1/rainbow_dinosaur__without_background__by_totallyrandomartist-d6i4uwy.png" })
+dino6 = Dinosaur.create!( { name: "Randomsaurus", age:rand(100), image_url:"http://pre00.deviantart.net/ebfd/th/pre/i/2013/226/d/1/rainbow_dinosaur__without_background__by_totallyrandomartist-d6i4uwy.png" })
+dino7 = Dinosaur.create!( { name: "Randomsaurus", age:rand(100), image_url:"http://pre00.deviantart.net/ebfd/th/pre/i/2013/226/d/1/rainbow_dinosaur__without_background__by_totallyrandomartist-d6i4uwy.png" })
+dino8 = Dinosaur.create!( { name: "Randomsaurus", age:rand(100), image_url:"http://pre00.deviantart.net/ebfd/th/pre/i/2013/226/d/1/rainbow_dinosaur__without_background__by_totallyrandomartist-d6i4uwy.png" })
