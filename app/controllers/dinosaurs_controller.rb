ccd795935de15c68d2f29510b432133c39600703
Tim Kocks <timhkocks@gmail.com>
2017-05-19 14:58:36 +0200

    Add form

--- app/controllers/dinosaurs_controller.rb
+++ app/controllers/dinosaurs_controller.rb
@@ -2,4 +2,21 @@ class DinosaursController < ApplicationController
   def index
     @dinosaurs = Dinosaur.all
   end
+
+  def show
+    @dinosaur = dinosaur.find(params[:id])
+  end
+
+  def new
+    @dinosaur = Dinosaur.new
+  end
+
+  def create
+    @dinosaur = Dinosaur.new(params.requie(:dinosaur).permit(:name, :age, :image_url))
+
+    if @dinosaur.save
+      redirect_to @dinosaurelse
+      render "new"
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 Rails.application.routes.draw do
   root "dinosaurs#index"
-  
+
   get "dinosaurs" => "dinosaurs#index"
+  get 'dinosaurs/new' => "dinosaurs#new", as: :new_dinosaur
+  get "dinosaurs/:id" => "dinosaurs#show", as: :dinosaur
+
+  post "dinosaurs" => "dinosaurs#create"
 end
